296e6ee12c063ad928ff9a679ce782dcc8505ac7
ayank <ayank@LAPTOP-CDMIKABR>
2024-09-20 12:43:48 +0530

    git commit

---
+++ actuator1/src/main/java/com/myfirstcontroller/CourseController.java
@@ -0,0 +1,21 @@
+package com.myfirstcontroller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+	@GetMapping("/courses")
+	public List<Course> getAllCourses() {
+		return Arrays.asList(
+				new Course(1,"Java","abc"),
+				new Course(2,"Angular","xyz")
+				
+				);
+	}
+	
+}
